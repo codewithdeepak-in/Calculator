47a21e863ab0577320e018f4c13648203cdea2e9
Deepak Chaudhary <101586061+codewithdeepak-in@users.noreply.github.com>
2023-08-31 20:07:33 +0530

    commit

--- calculator.js
+++
@@ -1,8 +0,0 @@
-// Define a Result that add character to the result section 
-function addToResult(char){
-    document.getElementById("result").value += char;
-}
-function calculateResult(){
-  const result = eval(document.getElementById("result").value);
-  document.getElementById("result").value = result;
-}
---
+++ script.js
@@ -0,0 +1,82 @@
+    // Define a Result that add character to the result section 
+    function addToResult(char){
+        document.getElementById("result").innerText += char;
+    }
+    function calculateResult(){
+      const result = eval(document.getElementById("result").innerText);
+      document.getElementById("result").innerText = result;
+    }
+    
+    function clearResult(){
+      document.getElementById('result').innerText="";
+    }
+    
+    function backResult(){
+      const val = document.getElementById('result').innerText
+      const valToArray = val.split('');
+      valToArray.pop();
+      let newString = valToArray.join("");
+      console.log(newString);
+      document.getElementById('result').innerText = newString;
+    }
+
+    document.addEventListener('keydown', function(event) {
+        const key = event.key;
+        switch (key) {
+          case '1':
+            addToResult('1');
+            break;
+          case '2':
+            addToResult('2');
+            break;
+          case '3':
+            addToResult('3');
+            break;
+          case '4':
+            addToResult('4');
+            break;
+          case '5':
+            addToResult('5');
+            break;
+          case '6':
+            addToResult('6');
+            break;
+          case '7':
+            addToResult('7');
+            break;
+          case '8':
+            addToResult('8');
+            break;
+          case '9':
+            addToResult('9');
+            break;
+          case '0':
+            addToResult('0');
+            break;
+          // Add cases for other numeric keys, operators, etc.
+          case '+':
+            addToResult('+');
+            break;
+          case '-':
+            addToResult('-');
+            break;
+          case '*':
+            addToResult('*');
+            break;
+          case '/':
+            addToResult('/');
+            break;
+          case '.':
+            addToResult('.');
+            break;
+          case 'Enter':
+            calculateResult();
+            break;
+          case 'Backspace':
+            backResult();
+            break;
+          case 'Delete':
+            clearResult();
+            break;
+        }
+      });
